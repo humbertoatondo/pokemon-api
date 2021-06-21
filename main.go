182b6cf6975b1d4f2a6624f28ec7fd4b4d5dd320
humbertoatondo <humberto_atondo@hotmail.com>
2021-06-21 05:01:58 -0700

    Test workflow

--- main.go
+++ main.go
@@ -1,28 +1,25 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
+	"os"
 
 	"github.com/humbertoatondo/pokemon-api/api"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// envPtr := flag.String("env", "development", "a string")
-	// flag.Parse()
-	// env := *envPtr
+	envPtr := flag.String("env", "development", "a string")
+	flag.Parse()
+	env := *envPtr
 
-	// fmt.Println(env)
+	fmt.Println(env)
 
-	// switch env {
-	// case "test":
-	// 	os.Setenv("pokemon_url", "http://localhost:5000/pokemon/")
-	// default:
-	// 	os.Setenv("pokemon_url", "https://pokeapi.co/api/v2/pokemon/")
-	// }
-
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatalf("Error laoding .env file")
+	switch env {
+	case "test":
+		os.Setenv("pokemon_url", "http://localhost:5000/pokemon/")
+	default:
+		os.Setenv("pokemon_url", "https://pokeapi.co/api/v2/pokemon/")
 	}
 
 	app := api.App{}
