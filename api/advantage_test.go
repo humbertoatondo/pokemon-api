2971fdc586868361a8211ff85d2079d84b697756
humbertoatondo <humberto_atondo@hotmail.com>
2021-06-21 02:31:29 -0700

    First advantages integration test

--- api/advantage_test.go
+++ api/advantage_test.go
@@ -1,8 +1,14 @@
 package api
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var app App
@@ -13,6 +19,37 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-// func TestComparePokemons(t *testing.T) {
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	app.Router.ServeHTTP(rr, req)
 
-// }
+	return rr
+}
+
+func TestComparePokemons(t *testing.T) {
+
+	t.Run("Comparing pikachu with lucario should return [false, true, false]", func(t *testing.T) {
+
+		urlPath := "http://localhost:5000/comparePokemons?pokemon1=pikachu&pokemon2=lucario"
+		response, _ := http.Get(urlPath)
+
+		var resBody advantage
+		if err := json.NewDecoder(response.Body).Decode(&resBody); err != nil {
+			t.Error(err)
+		}
+
+		jsonFilePath := "../testing_resources/advantages/pikachu_vs_lucario.json"
+		fileBytes, err := ioutil.ReadFile(jsonFilePath)
+		if err != nil {
+			t.Error(err)
+		}
+
+		var expectedBody advantage
+		json.Unmarshal(fileBytes, &expectedBody)
+
+		// fmt.Printf("%v\n", expectedBody)
+
+		assert.Equal(t, resBody, expectedBody)
+	})
+
+}
