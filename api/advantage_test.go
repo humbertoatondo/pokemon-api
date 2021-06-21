d1e719e95c1177e312150bb831e1b5d69bf9a42b
humbertoatondo <humberto_atondo@hotmail.com>
2021-06-21 07:50:34 -0700

    Add tests to ComparePokemons

--- api/advantage_test.go
+++ api/advantage_test.go
@@ -13,53 +13,53 @@ import (
 
 func TestComparePokemons(t *testing.T) {
 
-	// t.Run("Comparing pikachu with lucario should return [false, true, false]", func(t *testing.T) {
+	t.Run("Comparing pikachu with lucario should return [false, true, false]", func(t *testing.T) {
 
-	// 	urlPath := "http://localhost:5000/comparePokemons?pokemon1=pikachu&pokemon2=lucario"
-	// 	response, _ := http.Get(urlPath)
+		urlPath := "http://localhost:5000/comparePokemons?pokemon1=pikachu&pokemon2=lucario"
+		response, _ := http.Get(urlPath)
 
-	// 	var resBody advantage
-	// 	if err := json.NewDecoder(response.Body).Decode(&resBody); err != nil {
-	// 		t.Error(err)
-	// 	}
+		var resBody advantage
+		if err := json.NewDecoder(response.Body).Decode(&resBody); err != nil {
+			t.Error(err)
+		}
 
-	// 	jsonFilePath := "../testing_resources/advantages/pikachu_vs_lucario.json"
-	// 	fileBytes, err := ioutil.ReadFile(jsonFilePath)
-	// 	if err != nil {
-	// 		t.Error(err)
-	// 	}
+		jsonFilePath := "../testing_resources/advantages/pikachu_vs_lucario.json"
+		fileBytes, err := ioutil.ReadFile(jsonFilePath)
+		if err != nil {
+			t.Error(err)
+		}
 
-	// 	var expectedBody advantage
-	// 	json.Unmarshal(fileBytes, &expectedBody)
+		var expectedBody advantage
+		json.Unmarshal(fileBytes, &expectedBody)
 
-	// 	assert.Equal(t, resBody.ComparisonResults.DealsDoubleDamage, expectedBody.ComparisonResults.DealsDoubleDamage)
-	// 	assert.Equal(t, resBody.ComparisonResults.ReceivesHalfDamage, expectedBody.ComparisonResults.ReceivesHalfDamage)
-	// 	assert.Equal(t, resBody.ComparisonResults.ReceivesNoDamage, expectedBody.ComparisonResults.ReceivesNoDamage)
-	// })
+		assert.Equal(t, resBody.ComparisonResults.DealsDoubleDamage, expectedBody.ComparisonResults.DealsDoubleDamage)
+		assert.Equal(t, resBody.ComparisonResults.ReceivesHalfDamage, expectedBody.ComparisonResults.ReceivesHalfDamage)
+		assert.Equal(t, resBody.ComparisonResults.ReceivesNoDamage, expectedBody.ComparisonResults.ReceivesNoDamage)
+	})
 
-	// t.Run("Comparing lucario with lucario should return [true, true, false]", func(t *testing.T) {
+	t.Run("Comparing lucario with lucario should return [true, true, false]", func(t *testing.T) {
 
-	// 	urlPath := "http://localhost:5000/comparePokemons?pokemon1=lucario&pokemon2=lucario"
-	// 	response, _ := http.Get(urlPath)
+		urlPath := "http://localhost:5000/comparePokemons?pokemon1=lucario&pokemon2=lucario"
+		response, _ := http.Get(urlPath)
 
-	// 	var resBody advantage
-	// 	if err := json.NewDecoder(response.Body).Decode(&resBody); err != nil {
-	// 		t.Error(err)
-	// 	}
+		var resBody advantage
+		if err := json.NewDecoder(response.Body).Decode(&resBody); err != nil {
+			t.Error(err)
+		}
 
-	// 	jsonFilePath := "../testing_resources/advantages/lucario_vs_lucario.json"
-	// 	fileBytes, err := ioutil.ReadFile(jsonFilePath)
-	// 	if err != nil {
-	// 		t.Error(err)
-	// 	}
+		jsonFilePath := "../testing_resources/advantages/lucario_vs_lucario.json"
+		fileBytes, err := ioutil.ReadFile(jsonFilePath)
+		if err != nil {
+			t.Error(err)
+		}
 
-	// 	var expectedBody advantage
-	// 	json.Unmarshal(fileBytes, &expectedBody)
+		var expectedBody advantage
+		json.Unmarshal(fileBytes, &expectedBody)
 
-	// 	assert.Equal(t, resBody.ComparisonResults.DealsDoubleDamage, expectedBody.ComparisonResults.DealsDoubleDamage)
-	// 	assert.Equal(t, resBody.ComparisonResults.ReceivesHalfDamage, expectedBody.ComparisonResults.ReceivesHalfDamage)
-	// 	assert.Equal(t, resBody.ComparisonResults.ReceivesNoDamage, expectedBody.ComparisonResults.ReceivesNoDamage)
-	// })
+		assert.Equal(t, resBody.ComparisonResults.DealsDoubleDamage, expectedBody.ComparisonResults.DealsDoubleDamage)
+		assert.Equal(t, resBody.ComparisonResults.ReceivesHalfDamage, expectedBody.ComparisonResults.ReceivesHalfDamage)
+		assert.Equal(t, resBody.ComparisonResults.ReceivesNoDamage, expectedBody.ComparisonResults.ReceivesNoDamage)
+	})
 
 	t.Run("Comparing pikachu with ditto should return [false, false, false]", func(t *testing.T) {
 
