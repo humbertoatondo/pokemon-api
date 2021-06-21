b3aca5e1e6326efffb5f0717fa2d296ada353c36
humbertoatondo <humberto_atondo@hotmail.com>
2021-06-21 07:48:03 -0700

    Fix test Dokerfile

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"flag"
-	"os"
+	"log"
 
 	"github.com/humbertoatondo/pokemon-api/api"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	envPtr := flag.String("env", "development", "a string")
-	flag.Parse()
-	env := *envPtr
+	// envPtr := flag.String("env", "development", "a string")
+	// flag.Parse()
+	// env := *envPtr
 
-	switch env {
-	case "test":
-		os.Setenv("pokemon_url", "http://localhost:5000/pokemon/")
-	default:
-		os.Setenv("pokemon_url", "https://pokeapi.co/api/v2/pokemon/")
+	// switch env {
+	// case "test":
+	// 	os.Setenv("pokemon_url", "http://localhost:5000/pokemon/")
+	// default:
+	// 	os.Setenv("pokemon_url", "https://pokeapi.co/api/v2/pokemon/")
+	// }
+
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Error laoding .env file")
 	}
 
 	app := api.App{}
